17c62f68c81d3d9bb25fc83cf8f00b27030d64b3
ASverdlov <sverdlovalbert@gmail.com>
2016-04-25 23:19:13 +0300

    Changed build script to download Googletest

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Install googletest from github, if it doesn't exist
+if [ ! -d googletest ]; then
+    echo "Installing googletest.."
+    git clone https://github.com/google/googletest.git
+fi
+
 mkdir -p build
 cd build
 cmake .. || exit 0
