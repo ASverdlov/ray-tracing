e5999e2815f9f0b4ac0a8685bfab9a83eb696f25
tulindanil <tulindanil@gmail.com>
2016-08-14 01:26:18 +0300

    Add application's interface

---
+++ include/ray-tracing/application.hpp
@@ -0,0 +1,13 @@
+#include <renderer.hpp>
+#include <scene.hpp>
+#include <window.hpp>
+
+class Application {
+ public:
+  void Run();
+
+ private:
+  Renderer renderer_;
+  Window window_;
+  Scene scene_; 
+};
