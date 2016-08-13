a75e5f4992f8901d03612afae238b48ea425e633
tulindanil <tulindanil@gmail.com>
2016-08-14 01:54:31 +0300

    Delete camera's methods

--- include/ray-tracing/scene/camera.hpp
+++ include/ray-tracing/scene/camera.hpp
@@ -8,19 +8,7 @@ namespace rt {
 
 class Camera {
  public:
-  void SetPosition(const Vec3d&);
-  void SetPosition(double x, double y, double z);
 
-  void SetTransform(const Transform&);
-  void SetTransform(double x1, double y1, double z1,
-                    double x2, double y2, double z2,
-                    double x3, double y3, double z3);
-
-  void SetFocusDistance(double distance);
-
-  void SetFrameSize(double width, double height);
-
-  Line GetRay(double physical_x, double physical_y);
 
  private:
   Vec3d position_;
