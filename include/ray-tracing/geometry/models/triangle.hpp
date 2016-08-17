81be86258f2fa259c1bcdb8d2812c901b62a3938
ASverdlov <sverdlovalbert@gmail.com>
2016-08-17 16:00:51 +0300

    Fix naming of private member; Fix Vector class name

--- include/ray-tracing/geometry/models/triangle.hpp
+++ include/ray-tracing/geometry/models/triangle.hpp
@@ -2,17 +2,17 @@
 #define TRIANGLE_HPP
 
 #include <object.hpp>
-#include <basics.hpp>
+#include <vector.hpp>
 
 class Triangle : public Object {
  public:
-  void SetVertices(const Vec3d& v1, const Vec3d& v2, const Vec3d& v3);
+  void SetVertices(const Vector& v1, const Vector& v2, const Vector& v3);
   void SetVertices(double x1, double y1, double z1,
                    double x2, double y2, double z2,
                    double x3, double y3, double z3);
 
  private:
-  Vec3d vertices[3];
+  Vector vertices_[3];
 };
 
 #endif
