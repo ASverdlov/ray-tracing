d8915aa2bb3bc97a43b823b0c5db62ebe988cdc1
ASverdlov <sverdlovalbert@gmail.com>
2016-08-17 16:17:28 +0300

    Implement Window

---
+++ include/ray-tracing/window/window.hpp
@@ -0,0 +1,19 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+struct Resolution {
+  int width;
+  int height;
+};
+
+class Window {
+ public:
+   Window() {}
+   Window(int width, int height) : resolution_(width, height) {}
+   Window(const Resolution& resolution) : resolution_(resolution) {}
+
+ private:
+  Resolution resolution_;
+};
+
+#endif
