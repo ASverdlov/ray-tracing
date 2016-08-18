800462b8d493b2ac2fb39bfa9b000135ffc206c8
ASverdlov <sverdlovalbert@gmail.com>
2016-08-18 23:04:25 +0300

    Change Color struct method names

--- include/ray-tracing/render/color.hpp
+++ include/ray-tracing/render/color.hpp
@@ -6,21 +6,21 @@
 namespace rt {
 
 struct Color {
-  Color(float r, float g, float b)
-    : r(r), g(g), b(b)
-  {}
-  Color() : Color(0, 0, 0) {}
-
-  float r, g, b;
-
-  Color ApplyBrightness(float brightness) const;
-
   static const Color red;
   static const Color green;
   static const Color blue;
   static const Color white;
 
-  static const float brightnessBorder;
+  static const float brightness_treshold;
+
+  Color() : Color(0, 0, 0) {}
+  Color(float r, float g, float b)
+    : r(r), g(g), b(b)
+  {}
+
+  Color ApplyBrightness(float brightness) const;
+
+  float r, g, b;
 };
 
 std::ostream& operator<<(std::ostream&, const Color& color);
