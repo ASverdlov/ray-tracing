918ff50a3927131fffffe2d37ba0cc2726d833da
tulindanil <tulindanil@gmail.com>
2016-08-20 18:04:08 +0300

    Move Plane to class namespace

--- include/ray-tracing/scene/camera.hpp
+++ include/ray-tracing/scene/camera.hpp
@@ -11,6 +11,14 @@ class Camera : public Placeable {
   Ray GetRay(double x, double y);  // physical coordinates
   void Rotate(const Transform& transform) { transform_ = transform; }
  private:
+  struct Plane {
+    // Left up corner
+    Vector origin;
+    // That's a basis
+    Vector x;
+    Vector y;
+  }
+
   Transform transform_;
   double focus_distance_;
   double frame_width_;
