88e62e21cc2554c70247784b5e80cd5118abf265
tulindanil <tulindanil@gmail.com>
2016-08-21 02:32:13 +0300

    There is no need to clone googletest

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,6 @@
 PROJECT_ROOT="$(dirname $0)/.."
 cd $PROJECT_ROOT
 
-# Install Google.Test from Github
-if [ ! -d googletest ]; then
-    echo "Installing googletest.."
-    git clone https://github.com/google/googletest.git
-fi
-
 mkdir -p build
 cd build
 cmake .. && make
