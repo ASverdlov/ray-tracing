9fc71a91875956a4cc16bcf7b7864166cebeaf75
tulindanil <tulindanil@gmail.com>
2016-08-22 02:09:40 +0300

    Fix method declaration

--- include/ray-tracing/geometry/models/triangle.hpp
+++ include/ray-tracing/geometry/models/triangle.hpp
@@ -1,9 +1,9 @@
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
-#include "geometry/models/model.hpp"
-#include "geometry/ray.hpp"
-#include "geometry/vector.hpp"
+#include "ray-tracing/geometry/models/model.hpp"
+#include "ray-tracing/geometry/ray.hpp"
+#include "ray-tracing/geometry/vector.hpp"
 
 namespace rt {
 
@@ -14,7 +14,7 @@ class Triangle : public Model {
                    double x2, double y2, double z2,
                    double x3, double y3, double z3);
 
-  Collision Trace(const Ray&) override;
+  virtual Collision Trace(const Ray&) const;
 
  private:
   Vector vertices_[3];
