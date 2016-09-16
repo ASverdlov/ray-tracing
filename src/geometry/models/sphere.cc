0a3804d2117aaec1709f5a90ad6ade9229cabcec
ASverdlov <sverdlovalbert@gmail.com>
2016-09-16 03:07:54 +0300

    Fix typos

--- src/geometry/models/sphere.cc
+++ src/geometry/models/sphere.cc
@@ -1,10 +1,13 @@
+#include <vector>
+
 #include "ray-tracing/geometry/models/sphere.hpp"
 #include "ray-tracing/geometry/ray.hpp"
+#include "ray-tracing/geometry/vector.hpp"
 #include "ray-tracing/algebra/equation_solver.hpp"
 
 namespace rt {
 
-Collision Sphere::Trace(const Ray& ray) {
+Collision Sphere::Trace(const Ray& ray) const {
   /*
    * (x - center.x) ^ 2 + (y - center.y) ^ 2 + (z - center.z) ^ 2 = r ^ 2
    * x(t) = origin.x + direction.x * t
@@ -14,14 +17,14 @@ Collision Sphere::Trace(const Ray& ray) {
    *    (origin.y + direction.y * t - center.y) ^ 2 +
    *    (origin.z + direction.z * t - center.z) ^ 2 = r ^ 2
    */
-  const Ray& origin = ray.GetOrigin();
-  const Ray& direction = ray.GetDirection();
-  Equation equation = (Equation(0.0, direction.x, origin.x - center.x) ^ 2) +
-                      (Equation(0.0, direction.y, origin.y - center.y) ^ 2) +
-                      (Equation(0.0, direction.z, origin.z - center.z) ^ 2) +
-                       Equation(0.0, 0.0, -radius * radius);
+  const Vector& origin = ray.GetOrigin();
+  const Vector& direction = ray.GetDirection();
+  Equation equation = (Equation(0.0, direction.x, origin.x - position_.x) ^ 2) +
+                      (Equation(0.0, direction.y, origin.y - position_.y) ^ 2) +
+                      (Equation(0.0, direction.z, origin.z - position_.z) ^ 2) +
+                       Equation(0.0, 0.0, -radius_ * radius_);
 
-  vector<double> solutions;
+  std::vector<double> solutions;
   Solve(equation, back_inserter(solutions));
 
   Collision collision;
@@ -29,14 +32,14 @@ Collision Sphere::Trace(const Ray& ray) {
       double trace_distance = solution * direction.GetLength();
       Vector touching = origin + direction * solution;
 
-      Vector radius_vector = touching - center;
+      Vector radius_vector = touching - position_;
       double cosinus = (radius_vector * -direction) /
-                       (radius * direction.GetLength());
+                       (radius_ * direction.GetLength());
 
       // TODO(asverdlov, fix till 30.08.16):
       // Encapsulate following logic in Collision
       if (trace_distance <= 0 ||
-          trace_distance >= collision.distance ||
+          trace_distance >= collision.trace_distance ||
           cosinus <= 0) {
           continue;
       }
