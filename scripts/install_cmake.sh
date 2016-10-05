caf6ac91abf1b966db674431455c26bf575a5a31
tulindanil <tulindanil@gmail.com>
2016-10-05 22:19:15 +0300

    Rename fix

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -6,6 +6,6 @@ cd $PROJECT_ROOT
 wget https://cmake.org/files/v3.7/cmake-3.7.0-rc1.tar.gz && \
 gzip -d cmake-3.7.0-rc1.tar.gz && \
 tar -xf cmake-3.7.0-rc1.tar && \
-mv cmake* cmake && \
+mv cmake-3.7.0-rc1 cmake && \
 cd cmake && \
 ./bootstrap && make && sudo make install
