2f6bf1e3a1a0ea0c1b99993dfefbe59a10796b17
ASverdlov <sverdlovalbert@gmail.com>
2016-11-09 04:29:16 +0300

    Define Color const static members

---
+++ src/render/color.cc
@@ -0,0 +1,11 @@
+#include "ray-tracing/render/color.hpp"
+
+namespace rt {
+
+const Color Color::red(1, 0, 0);
+const Color Color::green(0, 1, 0);
+const Color Color::blue(0, 0, 1);
+const Color Color::white(1, 1, 1);
+const Color Color::black(0, 0, 0);
+
+}  // namespace rt
