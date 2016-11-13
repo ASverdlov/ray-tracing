b56dec2af7f903677cea43a3afa7c1481f5950f2
ASverdlov <sverdlovalbert@gmail.com>
2016-11-14 02:42:17 +0300

    Change OpenGL to ImageMagick

--- src/window/window.cc
+++ src/window/window.cc
@@ -1,7 +1,4 @@
-#ifdef _WIN32
-#include <windows.h>
-#endif
-#include <GLUT/glut.h>
+#include <Magick++.h>
 
 #include "ray-tracing/window/window.hpp"
 #include "ray-tracing/render/frame.hpp"
@@ -9,10 +6,15 @@
 namespace rt {
 
 void Window::Show(const Frame& frame) {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glDrawPixels(resolution_.width, resolution_.height,
-      GL_RGB, GL_FLOAT, frame.data());
-  glutSwapBuffers();
+  Magick::Image image(Magick::Geometry(resolution_.width, resolution_.height), 
+                      "white");
+  for (size_t x = 0; x < resolution_.width; ++x)
+    for (size_t y = 0; y < resolution_.height; ++y)
+      image.pixelColor(x, y, Magick::Color(
+                               frame[y * resolution_.width * 3 + x * 3 + 0],
+                               frame[y * resolution_.width * 3 + x * 3 + 1],
+                               frame[y * resolution_.width * 3 + x * 3 + 2]));
+  image.write("rendered_picture.png");
 }
 
 }  // namespace rt
