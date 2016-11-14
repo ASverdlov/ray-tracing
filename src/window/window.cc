ea840912e3717ce4e75d4f13a88bdf4ed3d1d7bc
ASverdlov <sverdlovalbert@gmail.com>
2016-11-14 03:05:17 +0300

    Fix syntax

--- src/window/window.cc
+++ src/window/window.cc
@@ -6,14 +6,14 @@
 namespace rt {
 
 void Window::Show(const Frame& frame) {
-  Magick::Image image(Magick::Geometry(resolution_.width, resolution_.height), 
+  Magick::Image image(Magick::Geometry(resolution_.width, resolution_.height),
                       "white");
   for (size_t x = 0; x < resolution_.width; ++x)
     for (size_t y = 0; y < resolution_.height; ++y)
       image.pixelColor(x, y, Magick::Color(
-                               frame[y * resolution_.width * 3 + x * 3 + 0],
-                               frame[y * resolution_.width * 3 + x * 3 + 1],
-                               frame[y * resolution_.width * 3 + x * 3 + 2]));
+                               65000 * frame[y * resolution_.width * 3 + x * 3 + 0],
+                               65000 * frame[y * resolution_.width * 3 + x * 3 + 1],
+                               65000 * frame[y * resolution_.width * 3 + x * 3 + 2]));
   image.write("rendered_picture.png");
 }
 
