863fbb6187ab3a3b9e176908c782cbf7188bec7d
ASverdlov <sverdlovalbert@gmail.com>
2016-11-14 03:26:08 +0300

    Fix syntax

--- src/window/window.cc
+++ src/window/window.cc
@@ -10,10 +10,11 @@ void Window::Show(const Frame& frame) {
                       "white");
   for (size_t x = 0; x < resolution_.width; ++x)
     for (size_t y = 0; y < resolution_.height; ++y)
-      image.pixelColor(x, y, Magick::Color(
-                               65000 * frame[y * resolution_.width * 3 + x * 3 + 0],
-                               65000 * frame[y * resolution_.width * 3 + x * 3 + 1],
-                               65000 * frame[y * resolution_.width * 3 + x * 3 + 2]));
+      image.pixelColor(x, y, 
+                       Magick::Color(
+                         65000 * frame[y * resolution_.width * 3 + x * 3 + 0],
+                         65000 * frame[y * resolution_.width * 3 + x * 3 + 1],
+                         65000 * frame[y * resolution_.width * 3 + x * 3 + 2]));
   image.write("rendered_picture.png");
 }
 
