9d52a270945137d598623ab815d71f8ef50b1749
ASverdlov <sverdlovalbert@gmail.com>
2016-11-17 21:30:28 +0300

    Add forgotten RenderTarget .hpp, .cc files

---
+++ include/ray-tracing/render/render_target.hpp
@@ -0,0 +1,23 @@
+#ifndef RENDER_TARGET_HPP
+#define RENDER_TARGET_HPP
+
+#include "ray-tracing/render/frame.hpp"
+#include "ray-tracing/utility.hpp"
+
+namespace rt {
+
+class RenderTarget {
+ public:
+  virtual void Draw(const Frame& frame) const = 0;
+  virtual void SetResolution(const Resolution& resolution);
+  virtual void SetResolution(float width, float height);
+  virtual size_t Width() const;
+  virtual size_t Height() const;
+
+ protected:
+  Resolution resolution_;
+};
+
+}  // namespace rt
+
+#endif
